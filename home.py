85907e4248f1df16cb1859abf5ef09901e7eb3f7
Kingfisher <Dargodl@gmail.com>
2014-11-03 19:43:15 -0500

    Fixed bootstrap css
    Fixed syntax error, added bootstrap css

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.rout('/')
+@app.route('/')
 @app.route('/login')
 def login():
     return render_template("login.html")
