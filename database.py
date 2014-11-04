e8e50db6692342e47262d1c2f9701466e6d0872d
DennisNenov <nycdennen@gmail.com>
2014-11-04 17:05:45 -0500

    Finished database code

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ connection = pymongo.Connection()
 
 db = connection["database"]
 users = db.users
+posts = db.posts
 
 #return true if valid input; false if not
 def checkPassword(passwordToCheck):
@@ -10,6 +11,9 @@ def checkPassword(passwordToCheck):
 
 def checkUsername(usernameToCheck):
     return len(usernameToCheck) > 0
+
+def checkPost(postToCheck):
+    return len(postToCheck) > 0
 
 def addUser(username, password):
     if (((checkUsername(username) == False) or (checkPassword(password) == False)) or (users.find({"username":username}).count()>0)):
@@ -24,7 +28,36 @@ def validateUser(username, password):
     if (record.count() != 1):
         return False
     else:
-        #print ""
-        #print record[0]['password']
         return record[0]['password'] == hashlib.sha512(password).hexdigest()
-       
+
+
+def addPost(username, post, privacy):
+    if ((checkPost(post) == False) or (users.find({"username":username}).count()<1)):
+        return False
+    else:
+        newPost = {"username": username,"post": post, "privacy": privacy}
+        posts.insert(newPost)
+        return True
+        
+def addPublicPost(username, post):
+    return addPost(username, post, "Public")    
+
+def addPrivatePost(username, post):
+    return addPost(username, post, "Private")
+
+def getPosts(privacy):
+    result = posts.find({'privacy': privacy})
+    postList = []
+    for post in result:
+        miniPostList = []
+        miniPostList.append(post['username'])
+        miniPostList.append(post['post'])
+        miniPostList.append(post['privacy'])
+        postList.append(miniPostList)
+    return postList
+        
+def getPublicPosts():
+    return getPosts("Public")
+
+def getPrivatePosts():
+    return getPosts("Private")
