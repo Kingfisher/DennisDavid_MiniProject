18468f1b1af20da5fd96be6f6078d3bb649de639
DennisNenov <nycdennen@gmail.com>
2014-11-04 20:51:11 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app.py
    	database.pyc

--- app.py
+++ app.py
@@ -11,18 +11,10 @@ def login():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-<<<<<<< HEAD
-        if (validate(username,password) == False ):
+        if (validate(username,password) == False):
             error = 'Unregistered username or incorrect password'
         flash("You've logged in successfully")
         return render_template("private.html")
-=======
-        if not database.validateUser(username,password):
-            flash('Unregistered username or incorrect password')
-            return render_template("login.html")
-        session['username'] = username
-        return redirect(url_for('private'))
->>>>>>> origin/master
     return render_template("login.html")
 
 @app.route('/signup', methods=["GET","POST"])
@@ -31,26 +23,18 @@ def signup():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-<<<<<<< HEAD
         if (database.addUser(username,password) == False):
             error = 'Unregistered username, too short username, or too short password'
-=======
-        if not database.addUser(username,password):
-            flash("Unregistered username, too short username, or too short password.")
->>>>>>> origin/master
             return render_template("signup.html")
         flash("Great! You've registered! Now you can log in.")
-        return redirect(url_for("login"))
+        return render_template("login.html")
     return render_template("signup.html")
 @app.route('/posts/private',methods=["GET","POST"])
 def public():
     return render_template("public.html")
 @app.route('/posts/public',methods=["GET","POST"])
 def private():
-    if 'username' in session:
-        return render_template("private.html")
-    flash('You are not logged in')
-    return redirect(url_for("login"))
+    return render_template("private.html")
 @app.route('/logout')
 def logout():
     session.pop('username', None)
